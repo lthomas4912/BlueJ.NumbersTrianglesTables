63d72fc54ec06b2f33f0b766de6a928092365acd
Leah <leah29726@gmail.com>
2019-02-12 22:14:52 -0500

    Changed files. Updated Triangles and Tables

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,52 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+       String result = "";
+        for(int i = 1; i <= 5; i++){
+           
+            for(int j = 1; j <= 5; j++){
+
+                int loop = (i*j);
+                result += String.format("%3d |",loop);
+                
+            }
+            result += "\n";
+        
+    }return result;
+        
+        
+        
     }
+        
 
     public static String getLargeMultiplicationTable() {
-        return null;
-    }
+         String result = "";
+        for(int i = 1; i <= 10; i++){
+           
+            for(int j = 1; j <= 10; j++){
+
+                int loop = (i*j);
+                result += String.format("%3d |",loop);
+                
+            }
+            result += "\n";
+        
+    }return result;
+}
+    
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for(int i = 1; i <= 20; i++){
+           
+            for(int j = 1; j <= 20; j++){
+
+                int loop = (i*j);
+                result += String.format("%3d |",loop);
+                
+            }
+            result += "\n";
+        
+    }return result;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -1,21 +1,46 @@
  
+import java.util.Scanner;
 
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+     String star = "*";
+     String result = "";
+      for(int width = 1; width <= numberOfStars; width++){
+        
+         result += star;
+        } 
+     
+     
+       return result;
+    
     }
+          
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        
+        String result = "";
+        
+        for(int i = 0 ; i < numberOfRows; i++){
+            for(int j = 0; j <= i; j++){
+                
+               result = result + "*";
+             
+               
+             
+              
+            } result = result + "\n";
+        }return result;
+        
+        
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
